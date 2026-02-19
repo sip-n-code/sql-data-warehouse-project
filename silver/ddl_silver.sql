d5f1505c339d66941dcd2399999ddf96312f2b46
sip-n-code <sipholukhele1994@gmail.com>
2026-02-19 12:18:53 +0200

    Add category id column to ddl

--- silver/ddl_silver.sql
+++ silver/ddl_silver.sql
@@ -13,14 +13,15 @@ IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
 GO
 
-CREATE TABLE silver.crm_cust_info (
-    cst_id              INT,
-    cst_key             NVARCHAR(50),
-    cst_firstname       NVARCHAR(50),
-    cst_lastname        NVARCHAR(50),
-    cst_marital_status  NVARCHAR(50),
-    cst_gndr            NVARCHAR(50),
-    cst_create_date     DATE,
+CREATE TABLE silver.crm_prd_info (
+    prd_id       INT,
+	cat_id		 NVARCHAR(50),
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
 	dwh_create_date		DATETIME2 DEFAULT GETDATE()
 );
 GO
